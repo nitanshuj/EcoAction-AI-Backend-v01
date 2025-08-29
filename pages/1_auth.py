d78539b5f57016b2a9ea6522ac0e18ef5d27ac07
Nitanshu Joshi <nitanshuj138@gmail.com>
2025-08-29 14:08:59 +0530

    streamlit hosting fix - try 2

--- pages/1_auth.py
+++ pages/1_auth.py
@@ -1,6 +1,17 @@
 # pages/1_auth.py
-import streamlit as st
+
+
 import sys
+
+try:
+    __import__('pysqlite3')
+    sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
+except ImportError:
+    # This will fail on Windows, which is fine, as it will use the system's sqlite3
+    pass
+# --- END FIX ---
+
+import streamlit as st
 import os
 
 # Add the parent directory to the path so we can import from utils

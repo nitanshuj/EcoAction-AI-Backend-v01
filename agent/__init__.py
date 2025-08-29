d2dd9d0ee3c2f3007b798989b6900b31b8d78a72
Nitanshu Joshi <nitanshuj138@gmail.com>
2025-08-29 18:05:34 +0530

    Streamlit hosting fix - try 3.6

--- agent/__init__.py
+++ agent/__init__.py
@@ -1,8 +1,8 @@
-# import sys
+# In agent/__init__.py
+import sys
 
-# try:
-#     __import__('pysqlite3')
-#     sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
-# except ImportError:
-#     # This will fail on Windows, which is fine, as it will use the system's sqlite3
-#     pass
+try:
+    __import__('pysqlite3')
+    sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
+except ImportError:
+    pass
